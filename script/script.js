a0844bbd8c7dcb142f2ed920dd092adaba2f06f6
Алик <nar.080@mail.ru>
2023-10-06 18:31:32 +0300

    score

--- script/script.js
+++ script/script.js
@@ -1,59 +1,76 @@
-var options = {
-  series: [{
-    name: 'Факт',
-    data: [1500, 5445, 5241, 6267]
-  }, {
-    name: 'Факт1',
-    data: [4440, 5875, 5541, 6447]
-  }, {
-    name: 'Факт1',
-    data: [5644, 3355, 4641, 5567]
-  }
-  ],
-  chart: {
-    type: 'bar',
+const data = [{
+  title: "Поступление",
+  value: 9999999,
+  hederTextBottom: 'План',
+  hederTextTop: 'Факт'
+}];
 
-  },
-  stroke: {
-    colors: ['transparent'],
-    width: 15,
 
-  },
-  dataLabels: {
-    enabled: false,
-  },
-  colors: ['#fb3f43', '#90c06c', '#2b9fdb'],
-  grid: {
-    show: true,
-    borderColor: '#90A4AE',
-    strokeDashArray: 3,
-    position: 'back',
-    xaxis: {
-      lines: {
-        show: true
-      }
-    },
-    yaxis: {
-      lines: {
-        show: true
-      }
-    },
-  },
-  yaxis: {
-    show: true,
-    min: 0,
-    max: 8000,
-    decimalsInFloat: undefined,
-    tickAmount: 8
-  },
-  xaxis: {
-    categories: ['Отель 1', 'Отель 2', 'Отель 3', 'Отель 4'
-    ],
 
-  },
+function chart() {
+  let element = $('<div>', {
+    class: 'score',
+    css: ({
+      width: "400px",
+      height: "200px",
+      margin: "95px auto",
+      background: "white",
+    })
+  }).appendTo($('body'))
+
+  let wrapper = $('<div>').css({
+    "borderTopLeftRadius": "5px",
+    "borderTopRightRadius": "5px",
+    "height": "40px",
+    "margin": "95px auto",
+    "background": "#babbbd",
+    "paddingLeft": "15px",
+    "paddingTop": "5px",
+    "fontSize": "25px"
+  }).appendTo(element);
+
+let textMeaningBootom = $('<div>').css({
+    "fontSize": "16px",
+    "display": "flex",
+      "flexDirection": "row",
+      "justifyContent": "space-evenly",
+      "margin-top":"-60px"
+
+  }).appendTo(element);
+
+  let txtKeyBootom = $('<div>').css({
+    "fontSize": "16px"
+  }).appendTo(textMeaningBootom);
+
+  let textMeaningTop = $('<div>').css({
+  
+    "fontSize": "16px",
+    "display": "flex",
+      "flexDirection": "row",
+      "justifyContent": "space-evenly",
+      "margin-top":"30px"
+  
+    
+  }).appendTo(element); // Заглавня 
+
+  let txtKeyTop = $('<div>').css({
+    "fontSize": "16px"
+  }).appendTo(textMeaningTop);
+
+
+
+  $('<span>').text(data[0].title).appendTo(wrapper);
+
+  $('<span>').text(data[0].value).appendTo(textMeaningTop);
+  $('<span>').text(data[0].hederTextTop).appendTo(txtKeyTop);
+
+  $('<span>').text(data[0].value).appendTo(textMeaningBootom);
+  $('<span>').text(data[0].hederTextBottom).appendTo(txtKeyBootom);
+  
+
 
 }
 
-var chart = new ApexCharts(document.querySelector("#chart"), options);
+chart()
 
-chart.render();
+
